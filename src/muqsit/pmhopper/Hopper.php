cac9ca387ec8bbc5f6d9d2d40fd55a9d40315836
Muqsit <hackhack05@gmail.com>
2021-11-04 13:12:11 +0000

    ops

--- src/muqsit/pmhopper/Hopper.php
+++ src/muqsit/pmhopper/Hopper.php
@@ -65,7 +65,6 @@ class Hopper extends VanillaHopper{
 	}
 
 	public function onScheduledUpdate() : void{
-		return;
 		$hopper_inventory = $this->getInventory();
 		if($hopper_inventory !== null){
 			$face = $this->getFacing();
